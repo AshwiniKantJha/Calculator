09b6a6c61427e6edd2e722d77e8ab411f30d9b47
Niraj Roy <85794375+Niraj-Roy@users.noreply.github.com>
2022-01-05 03:04:28 +0530

    Update einstein.js

--- einstein.js
+++ einstein.js
@@ -2,3 +2,10 @@ function myFunc(vmro) {
         document.getElementById(vmro);
         document.body.style.Color="lightgreen";
         }
+
+function changeclassstyle() {
+  var c = document.getElementsByClassName("buttons");
+  for (var i=0; i<c.length; i++) {
+    c[i].style.fill = "red";
+  }
+}
