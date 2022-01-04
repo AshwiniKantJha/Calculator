7ccc91c512ac6144942b9ce6bd5bf9c036b83610
Niraj Roy <85794375+Niraj-Roy@users.noreply.github.com>
2022-01-05 05:02:56 +0530

    Update einstein.js

--- einstein.js
+++ einstein.js
@@ -3,7 +3,7 @@ function myFunc() {
  document.body.style.backgroundColor="black";
   }
 
-let screen = document.getElementById('screen');
+let display = document.getElementById('screen');
 
 let buttons = Array.from(document.getElementsByClassName('btn'));
 
